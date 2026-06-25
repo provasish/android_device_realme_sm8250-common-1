d1b13ad8fcecfb0b2c583eb751472ac3ac211f80
Nolen Johnson <johnsonnolen@gmail.com>
2026-06-25 19:09:15 -0400

    sm8250-common: Use Clang r563880c for kernel build
    * Doesn't boot with default Clang version.
    
    Change-Id: I35765e296044eef428d01bbf5a21e91f1897704e

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -98,6 +98,7 @@ BOARD_RAMDISK_USE_LZ4 := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := BRAND_SHOW_FLAG=realme
 TARGET_KERNEL_SOURCE := kernel/realme/sm8250
 TARGET_KERNEL_CONFIG := vendor/kona-perf_defconfig vendor/oplus.config
+TARGET_KERNEL_CLANG_VERSION := r563880c
 TARGET_KERNEL_NO_GCC := true
 
 # Platform
